2169ac225bff48a1a176aa885c57b327fe45f870
nabbat <dubrov@me.com>
2023-07-30 16:21:13 +0200

    first increment is complited

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -53,8 +53,8 @@ func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // Простая функция для генерации уникального идентификатора
-func generateID(fullUrl string) string {
-	encodedStr := base64.URLEncoding.EncodeToString([]byte(fullUrl))
+func generateID(fullURL string) string {
+	encodedStr := base64.URLEncoding.EncodeToString([]byte(fullURL))
 	// Возвращаем первые 6 символов закодированной строки
 	if len(encodedStr) > 6 {
 		return encodedStr[:6]
