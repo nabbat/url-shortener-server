6b5ca7b6b4bbf41d953d327a9cbf3bb541d7a435
nabbat <dubrov@me.com>
2023-08-08 16:09:51 +0200

    iter5 test

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -2,6 +2,7 @@ package flags
 
 import (
 	flag "github.com/spf13/pflag"
+	"strings"
 )
 
 // Flags структура для хранения настроек
@@ -19,5 +20,8 @@ func ParseFlags() *Flags {
 	flag.StringVarP(&flg.ResultURL, "b", "b", "http://localhost:8080", "Адрес результирующего сокращённого URL.")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
+	if !strings.HasPrefix(flg.ResultURL, "http://") {
+		flg.ResultURL = "http://" + flg.ResultURL
+	}
 	return flg
 }
